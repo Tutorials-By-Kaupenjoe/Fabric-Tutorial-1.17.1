36e5e9c8ecaca4a3e6afb8e16b82b16bdd55fb1d
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-08-18 17:14:07 +0200

    added item

--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/TutorialMod.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/TutorialMod.java
@@ -1,13 +1,19 @@
 package net.tutorialsbykaupenjoe.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.tutorialsbykaupenjoe.tutorialmod.item.ModItems;
 
 public class TutorialMod implements ModInitializer {
+
+	public static final String MOD_ID = "tutorialmod";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		ModItems.registerModItems();
 
 		System.out.println("Hello Fabric world!");
 	}
---
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.tutorialsbykaupenjoe.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.tutorialsbykaupenjoe.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+    public static final Item RUBY = registerItem("ruby",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+}
